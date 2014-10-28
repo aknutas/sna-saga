41f825584c349fcd8a830d42ddc1fb412e3cdfb6
Antti Knutas <antti.knutas@iki.fi>
2014-10-29 01:32:04 +0200

    Implemented visualizations with a new library, D3.

--- js/visualize.js
+++ js/visualize.js
@@ -6,14 +6,14 @@ var Network = [];
 var centralityArray = [];
 
 //Hardcoded conf values, bad (but it works)
-var minNodeSize = 20;
-var maxNodeSize = 3*minNodeSize;
-
+var minNodeSize = 5;
+var maxNodeSize = 20;
+var nszDiff = maxNodeSize - minNodeSize;
 
 var maxNodeWeight = null;
 var minNodeWeight = null;
 
-function Node(id, target, weight) {
+function Edge(id, target, weight) {
     this.id = id;
     this.target = target;
     this.weight = weight;
@@ -37,110 +37,101 @@ function startLoad() {
 
 function loadReady(data) {
     $.each(data, function(index, value) {
-        var nodeX = new Node(value.source, value.target, value.weight);
-        Network.push(nodeX);
+        var newNode = new Edge(value.source, value.target, value.weight);
+        Network.push(newNode);
     });
     //Calculate centralities
+	//Takes Network as a parameter and stores the results in centralityArray
     goCalc(Network, centralityArray);
-    //Draw graph
-    drawGraph(Network);
+    //Draw graph based on network and centralityarray
+    drawNewStyleGraph(Network, centralityArray);
 }
 
-function drawGraph(graphsrc) {
+function drawNewStyleGraph(network, centralities) {
+	var width = 1024,
+	height = 768;
 
-    var graph = Viva.Graph.graph();
-    var graphics = Viva.Graph.View.svgGraphics(),
-        nodeSize = minNodeSize; // Default node size
+	var color = d3.scale.category20();
 
+	var force = d3.layout.force()
+	.charge(-500)
+	.linkDistance(100)
+	.size([width, height]);
 
+	var svg = d3.select("body").append("svg")
+	.attr("width", width)
+	.attr("height", height);
+	
+	var nodes = [],
+	indexednodes = [],
+	links = [];
+	
+	$.each(centralities, function(index, value){
+		//TODO: Adjustable filtering
+		//Checking to exclude from network
+		var toSkip = true;
+		network.forEach(function(link){
+			//TODO: Adjustable filtering
+			if((link.id === index || link.target === index) && link.weight > 1){
+				toSkip = false;
+			}
+		});
+		if(!toSkip){
+			//Adding new node to network (if passed filtering)
+			console.log("Added new node " + index + " weight: " + value);
+			var newnode = {"id" : index, "label" : index, "rweight" : value, "group" : 1};
+			nodes.push(newnode);
+			indexednodes[index] = newnode;
+		}
+	});
+	
+	network.forEach(function(link){
+		//TODO: Adjustable filtering
+		if(indexednodes[link.id] != null && indexednodes[link.target] != null && link.weight > 1){
+			var s = indexednodes[link.id],
+			t = indexednodes[link.target];
+			links.push({source: s, target: t});
+		}
+	});
 
-    var graphGenerator = Viva.Graph.generator();
-    var graph = graphGenerator.ladder(1000);
+	force
+	.nodes(nodes)
+	.links(links)
+	.start();
 
-    var graph = graphGenerator.grid(5, 6);
-    var layout = Viva.Graph.Layout.forceDirected(graph, {
-        springLength: 300, // link length
-        springCoeff: 10e-25,
-        dragCoeff: 0,
-        gravity: -0.6
-  //    theta: 1
-    });
+	var link = svg.selectAll(".link")
+	.data(links)
+	.enter().append("line")
+	.attr("class", "link")
+	.style("stroke-width", function(d) { return Math.sqrt(d.value); });
+	
+	// Create the groups under svg
+	var gnodes = svg.selectAll('g.gnode')
+	.data(nodes)
+	.enter()
+	.append('g')
+	.classed('gnode', true);
 
-    for (var i = 0; i < 150; ++i) {
-        layout.step();
-    }
+	var node = gnodes.append("circle")
+	.attr("class", "node")
+	.attr("r", function(d) { return ((((d.rweight-minNodeWeight)/(maxNodeWeight-minNodeWeight))*nszDiff)+minNodeSize); })
+	.style("fill", function(d) { return color(d.group); })
+	.call(force.drag);
+	
+	var labels = gnodes.append("text")
+	.attr("text-anchor", "middle")
+	.text(function(d) { return d.label; });
 
-    graphsrc.forEach(function(node) {
-        graphics.node(function(node) {
-            //Try to find and assign custom attributes
-            if(typeof centralityArray[node.id] != 'undefined'){
-                nodeSize = minNodeSize + ((maxNodeSize - minNodeSize) * (centralityArray[node.id]) / maxNodeWeight);
-            } else {
-                nodeSize = minNodeSize;
-            }
+	force.on("tick", function() {
+	    link.attr("x1", function(d) { return d.source.x; })
+	        .attr("y1", function(d) { return d.source.y; })
+	        .attr("x2", function(d) { return d.target.x; })
+	        .attr("y2", function(d) { return d.target.y; });
 
-            // This time it's a group of elements: http://www.w3.org/TR/SVG/struct.html#Groups
-            var ui = Viva.Graph.svg('g'),
-                // Create SVG text element with user id as content
-                svgText = Viva.Graph.svg('text')
-                .attr('font-size', 70)
-                .attr('stroke', 'white')
-                .attr('y', '-2px').text(node.id),
-
-                img = Viva.Graph.svg('image')
-                .attr('width', nodeSize)
-                .attr('height', nodeSize)
-                .link('https://secure.gravatar.com/avatar/' + node.data);
-            ui.append(svgText);
-            ui.append(img);
-
-            ui.addEventListener('click', function() {
-                // toggle pinned mode
-                layout.pinNode(node, !layout.isNodePinned(node));
-            });
-
-            return ui;
-
-        }).placeNode(function(nodeUI, pos) {
-            // 'g' element doesn't have convenient (x,y) attributes, instead
-            // we have to deal with transforms: http://www.w3.org/TR/SVG/coords.html#SVGGlobalTransformAttribute
-            var pozX = (pos.x - nodeSize / 2);
-            var pozY = (pos.y - nodeSize / 2);
-
-            nodeUI.attr('transform',
-                'translate(' +
-                (pozX) + ',' + (pozY) + ')');
-
-        });
-
-        graph.addLink(node.id, node.target);
-
-        graphics.link(function(link) {
-
-            return Viva.Graph.svg('path')
-                //.attr('length', 10)
-                .attr('stroke', 'maroon') //  #656565 dark grey
-                .attr('stroke-width', 7)
-                .attr('stroke-dasharray', '3, 4');
-        }).placeLink(function(linkUI, fromPos, toPos) {
-            // linkUI - is the object returend from link() callback above.
-            var data = 'M' + fromPos.x + ',' + fromPos.y +
-                'L' + toPos.x + ',' + toPos.y;
-            // 'Path data' (http://www.w3.org/TR/SVG/paths.html#DAttribute )
-            // is a common way of rendering paths in SVG:
-            linkUI.attr("d", data);
-        });
-    });
-
-    var renderer = Viva.Graph.View.renderer(graph, {
-        layout: layout,
-        graphics: graphics,
-        renderLinks: true,
-        prerender: true
-        //container : document.getElementById('graphContainer')
-    });
-    renderer.run();
-
+			gnodes.attr("transform", function(d) { 
+			        return 'translate(' + [d.x, d.y] + ')'; 
+			    });
+	  });
 }
 
 function goCalc(calcNet, centResults) {
@@ -157,7 +148,6 @@ function goCalc(calcNet, centResults) {
     var counter = 0;
     var newTableRow;
     
-    
     $.each(betweenness, function(index, value) {
         // Store results
         centResults[value.key] = value.value;
@@ -172,7 +162,7 @@ function goCalc(calcNet, centResults) {
             minNodeWeight = value.value;
         }
         
-        // Start drawing HTML
+        // Start drawing the HTML table
         if ((counter % 2) === 0) {
             newTableRow = $('<tr>');
             newTableRow.html("<td>" + value.key + "</td>" + "<td>" + value.value + "</td>");
